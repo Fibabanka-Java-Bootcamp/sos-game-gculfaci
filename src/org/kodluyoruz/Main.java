709e6c5e73d4e6c5e02cc9f8db0f7280b05ea916
gculfaci <gculfaci@gmail.com>
2020-12-27 03:19:48 +0300

    RowColumn, PlayerTurn,SandO controls added

--- src/org/kodluyoruz/Main.java
+++ src/org/kodluyoruz/Main.java
@@ -1,6 +1,4 @@
-
 import java.util.Scanner;
-//import java.util.Random;
 
 
 public class Main {
@@ -31,6 +29,8 @@ public class Main {
                 if(newEnteredValue < 9 && newEnteredValue > 3){
                     SosTable newTable = new SosTable();
                     newTable.sizeOfMatrix = newEnteredValue;
+
+                    System.out.println(" ");
                     System.out.println("** GAME TABLE **");
                     updatedTable = newTable.createSosTable(newTable.sizeOfMatrix);
                     whileLoop = false;
@@ -38,38 +38,99 @@ public class Main {
                     int NumberOfMove=(enteredValue*enteredValue);
 
                     Player controller = new Player();
+                    Player player = new Player();
                     int turnToPlay = controller.whoStartToPlay();
                     
                     while(0 < NumberOfMove){
 
-                        Player player = new Player();
-
-                        int PlayerCoordinateX = player.playerMoveX(turnToPlay,enteredValue);
-                        int PlayerCoordinateY = player.playerMoveY(turnToPlay,enteredValue);
                         
-                        String selectedLetterFromComputer = controller.selectLetter(turnToPlay);
-                       
-                        /* COMPUTER PLAYING
-                        String abc = "SO";
+                        if (turnToPlay == 1){
+                            System.out.println("Your Turn");
+                            int PlayerCoordinateX = player.playerMoveX(turnToPlay,enteredValue);
+                            int PlayerCoordinateY = player.playerMoveY(turnToPlay,enteredValue);
 
-                        char letter = abc.charAt(rd.nextInt(abc.length()));
-                        String selectedOneOfOandS = String.valueOf(letter);*/
+                        
+                            String selectedLetter = controller.selectLetter(turnToPlay);
 
-                        if(  (0 < PlayerCoordinateX) && (PlayerCoordinateX < newEnteredValue) &&
-                             (0 < PlayerCoordinateY) && (PlayerCoordinateY < newEnteredValue) ){
-                            updatedTable[PlayerCoordinateX][PlayerCoordinateY]= selectedLetterFromComputer;
-                            SosTable.updateTable (updatedTable,newEnteredValue);
+                            if(  (0 < PlayerCoordinateX) && (PlayerCoordinateX < newEnteredValue) && (0 < PlayerCoordinateY) && (PlayerCoordinateY < newEnteredValue) && (selectedLetter.matches("[S,s,O,o]")) ){
+                                
+                                if(updatedTable[PlayerCoordinateX][PlayerCoordinateY] == "-"){
+                                    updatedTable[PlayerCoordinateX][PlayerCoordinateY]= selectedLetter;
+                                    SosTable.updateTable (updatedTable,newEnteredValue);
+                                    System.out.println(" ");
+                                    NumberOfMove--;
+                                    turnToPlay = 2;
+                                }
+                                else{
+                                    System.out.println("WARNING! Selected coordinates are full. Please try to select empty coordinates");
+                                    System.out.println(" ");
+
+                                }
+
+                                
+
+                            }
+                            else if( ( (0 < PlayerCoordinateX) && (PlayerCoordinateX < newEnteredValue) && (0 < PlayerCoordinateY) && (PlayerCoordinateY < newEnteredValue) ) && !(selectedLetter.matches("[S,s,O,o]"))){
+
+                                System.out.println("WARNING! Please try to enter just S or O");
+                                System.out.println(" ");
+                            }
+
+                            else if( !( (0 < PlayerCoordinateX) && (PlayerCoordinateX < newEnteredValue) && (0 < PlayerCoordinateY) && (PlayerCoordinateY < newEnteredValue) ) && (selectedLetter.matches("[S,s,O,o]"))){
+
+                                System.out.println("WARNING! Please enter a value between 1 and "+ enteredValue+ " for row and column");
+                                System.out.println(" ");
+                            }
+
+                            else{
+                                System.out.println("Both the entered coordinates and the character are incorrect. Please try again.");
+                                System.out.println(" ");
+
+                            }
 
                         }
-                        else System.out.println("HATALI GİRDİN TEKRAR KOORDİNAT GİR");
 
+                        if(turnToPlay == 2){
+                            System.out.println("Computer Turn");
+                            int PlayerCoordinateX = player.playerMoveX(turnToPlay,enteredValue);
+                            int PlayerCoordinateY = player.playerMoveY(turnToPlay,enteredValue);
+
+                            if(updatedTable[PlayerCoordinateX][PlayerCoordinateY] == "-"){
+                                
+                                String selectedLetterFromComputer = controller.selectLetter(turnToPlay);
+                                updatedTable[PlayerCoordinateX][PlayerCoordinateY]= selectedLetterFromComputer;
+                                SosTable.updateTable (updatedTable,newEnteredValue);
+                                System.out.println(" ");
+                                NumberOfMove--;
+                                turnToPlay = 1;
+
+                            }
+                            else{
+                                System.out.println("Computer searching empty coordinates ...");
+                                System.out.println(" ");
+
+                            }
+                            
                         
-                        NumberOfMove--;
+                            
+
+
+
+                            
+
+  
+
+                        }
+                        
+
                     }
                 
                 }
                 
-                else  System.out.print("WARNING! TRY TO CHOOSE ONE OF 3-4-5-6-7 :");
+                else{
+                    System.out.print("WARNING! TRY TO CHOOSE ONE OF 3-4-5-6-7 :");
+
+                }  
             
             }
             else{
--- src/org/kodluyoruz/Player.java
+++ src/org/kodluyoruz/Player.java
@@ -1,5 +1,3 @@
-
-
 import java.util.Scanner;
 import java.util.Random;
 
@@ -13,16 +11,16 @@ public class Player {
     public int playerMoveX(int turnOfPlayer, int enteredValue){
 
         if(turnOfPlayer==1){
-
-            System.out.println("Please enter row number: ");
+            System.out.print("Please enter row number: ");
             String PlayerMoveX = scanner.nextLine();
             int PlayerCoordinateX = Integer.parseInt(PlayerMoveX);
             return PlayerCoordinateX;
+  
         }
         else{
-            System.out.println("Computer selected row number");
-            //Random randomRow = new Random();
+            
             int PlayerCoordinateX = rd.nextInt(enteredValue)+1;
+            System.out.println("Computer selected row number: "+ PlayerCoordinateX);
             return PlayerCoordinateX;
         }
         
@@ -30,16 +28,15 @@ public class Player {
 
     public int playerMoveY(int turnOfPlayer, int enteredValue){
         if(turnOfPlayer==1){
-
-            System.out.println("Please enter column number:  ");
+            System.out.print("Please enter column number:  ");
             String PlayerMoveY = scanner.nextLine();
             int PlayerCoordinateY = Integer.parseInt(PlayerMoveY);
             return PlayerCoordinateY;
         }
         else{
-            System.out.println("Computer selected column ");
-            //Random randomColumn = new Random();
+            
             int PlayerCoordinateY = rd.nextInt(enteredValue)+1;
+            System.out.println("Computer selected column number: "+ PlayerCoordinateY);
             return PlayerCoordinateY;
 
         }
@@ -52,18 +49,33 @@ public class Player {
         Random randomDice = new Random();
 		int dice1 = randomDice.nextInt(6)+1;
         int dice2 = randomDice.nextInt(6)+1;
+        System.out.println(" ");
         System.out.println("THE DICE ARE ROLLED ... ");
 
-        if(dice1>= dice2){
+        if(dice1 > dice2){
 
+            System.out.println("YOU "+ dice1 +" - " + dice2 + " COMPUTER");
+            System.out.println("YOU WON");
+            System.out.println(" ");
             turnNumber = 1;
-            System.out.println("YOU WON. IT IS YOUR TURN TO PLAY");
             return turnNumber;
+        }
+        else if(dice1 == dice2){
+
+            System.out.println("YOU "+ dice1 +" - " + dice2 + " COMPUTER");
+            System.out.println("DRAW. THE COMPUTER LET YOU TO START GAME ");
+            System.out.println(" ");
+            turnNumber = 1;
+            return turnNumber;
+
+
         } 
         
         else{
-            System.out.println("YOU LOST. IT IS COMPUTER TURN TO PLAY");
-            turnNumber = 1;
+            System.out.println("YOU "+ dice1 +" - " + dice2 + " COMPUTER");
+            System.out.println("YOU LOST");
+            System.out.println(" ");
+            turnNumber = 2;
             return turnNumber;
         } 
 
@@ -74,22 +86,25 @@ public class Player {
     public String selectLetter(int turnOfPlayer){
         if(turnOfPlayer==1){
 
-            System.out.println("Please select one of S or O: ");  
+            System.out.print("Please select one of S or O: ");  
             String selectedLetter = scanner.nextLine();
+            System.out.println(" ");
+            selectedLetter = selectedLetter.toUpperCase();
             return selectedLetter;
 
         }
         else{
-            System.out.println("Computer selected column ");
+            
             String abc = "SO";
 
             char letter = abc.charAt(rd.nextInt(abc.length()));
             String selectedLetter = String.valueOf(letter);
+            System.out.println("Computer selected "+ selectedLetter);
+            System.out.println(" ");
             return selectedLetter;
 
         }
 
     }
-
     
 }
